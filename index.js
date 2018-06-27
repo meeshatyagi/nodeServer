f0ef1a62dbd4e6bd23f27806334a24a69473ebf3
meeshatyagi <tyagimish@gmail.com>
2018-06-28 00:09:39 +0530

    simple node example

---
+++ index.js
@@ -0,0 +1,22 @@
+var react = {
+    perimeter : (x,y) => (2*(x+y)),
+    area : (x,y) => (x*y)
+}
+
+function solveReact(l,b){
+    console.log("area of retangle"+l);
+    console.log("area of retangle"+b);
+
+    if(l <= 0 && b <= 0){
+        console.log("illegal value");
+    }else{
+        console.log("calculated area"+react.area(l,b));
+        console.log("calculated perimeter"+react.perimeter(l,b));
+    }
+
+}
+
+solveReact(2,3);
+solveReact(3,3);
+solveReact(9,2);
+solveReact(2,-9);
