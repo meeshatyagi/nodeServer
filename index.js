6056d503645d8ae519ebbd7e696853cf188ac500
meeshatyagi <tyagimish@gmail.com>
2018-06-28 00:37:10 +0530

    simple node module

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var react = {
-    perimeter : (x,y) => (2*(x+y)),
-    area : (x,y) => (x*y)
-}
+var react = require('./rectangle');    
 
 function solveReact(l,b){
     console.log("area of retangle"+l);
     console.log("area of retangle"+b);
 
-    if(l <= 0 && b <= 0){
+    if(l <= 0 || b <= 0){
         console.log("illegal value");
     }else{
         console.log("calculated area"+react.area(l,b));
